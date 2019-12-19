5bac4dc905af1dc4bb01d76c8ec0922c99f4b86d
Ekata <yogaelen108@gmail.com>
2019-12-19 21:36:30 +0000

    added top rated and top commented movies section

--- src/main.js
+++ src/main.js
@@ -2,8 +2,9 @@ import {renderUserLevel} from './components/user-level'
 import {renderMenu} from './components/menu'
 import {generateMoviesCards} from './mock/movie-card'
 import {generateMenu} from './mock/menu'
-import {renderMovieCard, generateMovieCard} from './components/movie-card'
+import {renderMovieCard} from './components/movie-card'
 import {renderSnowMoreButton} from './components/show-more-button'
+import {renderMovieList} from './components/extra-movie-list'
 import {renderExtraMovieDetails} from './components/extra-movie-details'
 
 const MOVIES_COUNT = 5;
@@ -14,11 +15,12 @@ const renderMoviesContainer = () => {
     <section class="films-list">
       <h2 class="films-list__title visually-hidden">All movies. Upcoming</h2>
 
-      <div class="films-list__container">
+      <div class="films-list__container"></div>
       </section>
     </section>`
   );
 };
+
 
 const render = (container, template, place = `beforeend`) => {
   container.insertAdjacentHTML(place, template);
@@ -36,6 +38,29 @@ const moviesContainerElement = mainElement.querySelector(`.films-list__container
 
 cards.forEach(card => render(moviesContainerElement, renderMovieCard(card)));
 
-render(mainElement, renderSnowMoreButton());
-render(mainElement, renderExtraMovieDetails(cards[0]));
-console.log(cards[0])
+const moviesListSection = mainElement.querySelector(`.films-list`);
+render(moviesListSection, renderSnowMoreButton());
+
+const moviesExtraList = mainElement.querySelector(`.films`);
+render(moviesExtraList, renderMovieList(`Top Rated`));
+render(moviesExtraList, renderMovieList(`Most Commented`));
+
+const getTopRatedMovies = (cards) => {
+  const sortedCards = cards.slice().sort((a, b) => b.rating - a.rating);
+  return sortedCards.slice(0, 2);
+};
+
+const getTopCommentedMovies = (cards) => {
+  const sortedCards = cards.slice().sort((a, b) => b.comments - a.comments);
+  return sortedCards.slice(0, 2);
+};
+
+const topRatedMovies = getTopRatedMovies(cards);
+const topCommentedMovies = getTopCommentedMovies(cards);
+
+const movieLists = mainElement.querySelectorAll(`.films-list--extra .films-list__container`);
+topRatedMovies.forEach(card => render(movieLists[0], renderMovieCard(card)));
+topCommentedMovies.forEach(card => render(movieLists[1], renderMovieCard(card)));
+
+//render(mainElement, renderExtraMovieDetails(cards[0]));
+
