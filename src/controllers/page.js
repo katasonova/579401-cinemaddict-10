f90fe96a44c0374384a99180eaa2a509183f02a4
Ekata <yogaelen108@gmail.com>
2020-01-06 19:38:05 +0000

    implemented sorting feature

--- src/controllers/page.js
+++ src/controllers/page.js
@@ -1,5 +1,6 @@
 import {render} from '../utils/render';
 import {isEscWasPressed} from '../utils/common';
+import Sort, {SortingType} from '../components/sort';
 import MoviesList from '../components/movies-list';
 import NoMovies from '../components/no-movies';
 import Card from '../components/movie-card';
@@ -52,9 +53,14 @@ const renderCard = (container, card) => {
 export default class PageController {
   constructor(container) {
     this._container = container;
+    this._sort = new Sort();
     this._moviesList = new MoviesList();
     this._noMovies = new NoMovies();
     this._showMoreButton = new SnowMoreButton();
+  }
+
+  renderCards(container, array) {
+    array.slice(0, INITIAL_MOVIES_NUMBER).forEach((card) => renderCard(container, card));
   }
 
   render(cards) {
@@ -64,12 +70,33 @@ export default class PageController {
       return;
     }
 
+    render(container, this._sort.getElement());
+
     render(container, this._moviesList.getElement());
 
     const moviesContainerElement = container.querySelector(`.films-list__container`);
-    let presentMoviesNumber = INITIAL_MOVIES_NUMBER;
 
-    cards.slice(0, INITIAL_MOVIES_NUMBER).forEach((card) => renderCard(moviesContainerElement, card));
+    let sortedCards = cards;
+    const sortHandler = (type) => {
+      moviesContainerElement.innerHTML = ``;
+
+      switch (type) {
+        case SortingType.DATE:
+          sortedCards = cards.slice().sort((a, b) => b.year - a.year);
+          break;
+        case SortingType.RATING:
+          sortedCards = cards.slice().sort((a, b) => b.rating - a.rating);
+          break;
+        case SortingType.DEFAULT:
+          sortedCards = cards.slice();
+          break;
+      }
+      this.renderCards(moviesContainerElement, sortedCards);
+      render(moviesListSection, this._showMoreButton.getElement());
+    };
+    this._sort.setSortingTypeClickHandler(sortHandler);
+
+    this.renderCards(moviesContainerElement, sortedCards);
 
     const moviesListSection = container.querySelector(`.films-list`);
     render(moviesListSection, this._showMoreButton.getElement());
@@ -78,13 +105,13 @@ export default class PageController {
     render(container, new MovieList(`Most Commented`).getElement());
 
     const getTopRatedMovies = () => {
-      const sortedCards = cards.slice().sort((a, b) => b.rating - a.rating);
-      return sortedCards.slice(0, 2);
+      const sortedCardsByRating = cards.slice().sort((a, b) => b.rating - a.rating);
+      return sortedCardsByRating.slice(0, 2);
     };
 
     const getTopCommentedMovies = () => {
-      const sortedCards = cards.slice().sort((a, b) => b.comments - a.comments);
-      return sortedCards.slice(0, 2);
+      const sortedCardsByComments = cards.slice().sort((a, b) => b.comments - a.comments);
+      return sortedCardsByComments.slice(0, 2);
     };
 
     const topRatedMovies = getTopRatedMovies(cards);
@@ -94,14 +121,16 @@ export default class PageController {
     topRatedMovies.forEach((card) => renderCard(movieLists[0], card));
     topCommentedMovies.forEach((card) => renderCard(movieLists[1], card));
 
+    let presentMoviesNumber = INITIAL_MOVIES_NUMBER;
     const showMoreButtonClickHandler = () => {
       const renderedMovies = presentMoviesNumber;
       presentMoviesNumber += MOVIES_TO_LOAD_MORE;
 
-      cards.slice(renderedMovies, presentMoviesNumber).forEach((card) => renderCard(moviesContainerElement, card));
+      sortedCards.slice(renderedMovies, presentMoviesNumber).forEach((card) => renderCard(moviesContainerElement, card));
 
-      if (presentMoviesNumber >= cards.length) {
+      if (presentMoviesNumber >= sortedCards.length) {
         this._showMoreButton.getElement().remove();
+        presentMoviesNumber = INITIAL_MOVIES_NUMBER;
       }
     };
 
