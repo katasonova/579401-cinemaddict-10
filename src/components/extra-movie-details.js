b130c512425ec45ee153a6fadab761c8e12e9c12
Ekata <yogaelen108@gmail.com>
2020-01-19 17:49:56 +0000

    implemented SmartComponent class

--- src/components/extra-movie-details.js
+++ src/components/extra-movie-details.js
@@ -1,4 +1,4 @@
-import AbstractComponent from './abstract';
+import AbstractSmartComponent from './smart-abstract';
 
 const renderGenres = (genres) => {
   return Array.from(genres).map((el) => {
@@ -8,7 +8,61 @@ const renderGenres = (genres) => {
   }).join(``);
 };
 
-const createExtraMovieDetailsTemplate = (card) => {
+const renderRatingTemplate = (card) => {
+  return (
+    `<div class="form-details__middle-container">
+    <section class="film-details__user-rating-wrap">
+      <div class="film-details__user-rating-controls">
+        <button class="film-details__watched-reset" type="button">Undo</button>
+      </div>
+
+      <div class="film-details__user-score">
+        <div class="film-details__user-rating-poster">
+          <img src="${card.poster}" alt="film-poster" class="film-details__user-rating-img">
+        </div>
+
+        <section class="film-details__user-rating-inner">
+          <h3 class="film-details__user-rating-title">${card.title}</h3>
+
+          <p class="film-details__user-rating-feelings">How you feel it?</p>
+
+          <div class="film-details__user-rating-score">
+            <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="1" id="rating-1">
+            <label class="film-details__user-rating-label" for="rating-1">1</label>
+
+            <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="2" id="rating-2">
+            <label class="film-details__user-rating-label" for="rating-2">2</label>
+
+            <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="3" id="rating-3">
+            <label class="film-details__user-rating-label" for="rating-3">3</label>
+
+            <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="4" id="rating-4">
+            <label class="film-details__user-rating-label" for="rating-4">4</label>
+
+            <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="5" id="rating-5">
+            <label class="film-details__user-rating-label" for="rating-5">5</label>
+
+            <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="6" id="rating-6">
+            <label class="film-details__user-rating-label" for="rating-6">6</label>
+
+            <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="7" id="rating-7">
+            <label class="film-details__user-rating-label" for="rating-7">7</label>
+
+            <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="8" id="rating-8">
+            <label class="film-details__user-rating-label" for="rating-8">8</label>
+
+            <input type="radio" name="score" class="film-details__user-rating-input visually-hidden" value="9" id="rating-9" checked>
+            <label class="film-details__user-rating-label" for="rating-9">9</label>
+
+          </div>
+        </section>
+      </div>
+    </section>
+  </div>`
+  );
+};
+
+const createExtraMovieDetailsTemplate = (card, options = {}) => {
   return (
     `<section class="film-details">
     <form class="film-details__inner" action="" method="get">
@@ -72,16 +126,17 @@ const createExtraMovieDetailsTemplate = (card) => {
           </div>
         </div>
         <section class="film-details__controls">
-        <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist">
+        <input type="checkbox" class="film-details__control-input visually-hidden" id="watchlist" name="watchlist" ${options.isInWatchlist ? `checked` : ``}>
         <label for="watchlist" class="film-details__control-label film-details__control-label--watchlist">Add to watchlist</label>
 
-        <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched">
+        <input type="checkbox" class="film-details__control-input visually-hidden" id="watched" name="watched" ${options.isWatched ? `checked` : ``}>
         <label for="watched" class="film-details__control-label film-details__control-label--watched">Already watched</label>
 
-        <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite">
+        <input type="checkbox" class="film-details__control-input visually-hidden" id="favorite" name="favorite" ${options.isFavourite ? `checked` : ``}>
         <label for="favorite" class="film-details__control-label film-details__control-label--favorite">Add to favorites</label>
       </section>
       </div>
+      ${card.isWatched ? renderRatingTemplate(card) : ``}
         </section>
       </div>
     </form>
@@ -89,14 +144,48 @@ const createExtraMovieDetailsTemplate = (card) => {
   );
 };
 
-export default class ExtraMovieDetails extends AbstractComponent {
+export default class ExtraMovieDetails extends AbstractSmartComponent {
   constructor(card) {
     super();
     this._card = card;
+    this._isInWatchlist = !!card.isInWatchlist;
+    this._isWatched = !!card.isWatched;
+    this._isFavourite = !!card.isFavourite;
+  }
+
+  recoveryListeners() {
+    this._subscribeOnEvents();
+  }
+
+  rerender() {
+    super.rerender();
+  }
+
+  _subscribeOnEvents() {
+    const element = this.getElement();
+
+    element.querySelector(`.film-details__control-label--watchlist`).addEventListener(`click`, () => {
+        this._isInWatchlist = !this._isInWatchlist;
+        this.rerender();
+    });
+
+    element.querySelector(`.film-details__control-label--watched`).addEventListener(`click`, () => {
+        this._isWatched = !this._isWatched;
+        this.rerender();
+    });
+
+      element.querySelector(`.film-details__control-label--favorite`).addEventListener(`click`, () => {
+        this._isWatched = !this._isWatched;
+        this.rerender();
+    });
   }
 
   getTemplate() {
-    return createExtraMovieDetailsTemplate(this._card);
+    return createExtraMovieDetailsTemplate(this._card, {
+      isInWatchlist: this._isInWatchlist,
+      isWatched: this._isWatched,
+      isFavourite: this._isFavourite,
+    });
   }
 
   setAddToWatchlistButtonClickHandler(handler) {
